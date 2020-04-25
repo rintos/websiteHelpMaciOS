39ffd6b50052a53b52994a89b8997de221a071d9
Victor Soares <victor.soares@redfox.tech>
2020-04-25 16:05:31 -0300

    update navbar2

--- src/Header.js
+++ src/Header.js
@@ -3,40 +3,19 @@ import LinkWrapper from './LinkWrapper';
 
 const Header = () => {
 
-    document.addEventListener('DOMContentLoaded', function() {
-        var elems = document.querySelectorAll('.sidenav');
-        var instances = M.Sidenav.init(elems, options);
-      });
-
     return (
         <nav>
             <div class="nav-wrapper indigo lighten-2">
-                <LinkWrapper to="/" class="brand-logo right">HelpMaciOS</LinkWrapper>
-                <a href="#" data-target="mobile-demo" class="sidenav-trigger"><i class="material-icons">menu</i></a>
-                <ul id="nav-mobile" class="left hide-on-med-and-down">
+                <LinkWrapper to="/" class="brand-logo">HelpMaciOS</LinkWrapper>
+                <ul id="nav-mobile" class="right hide-on-med-and-down">
                     <li><LinkWrapper to="/home">List of Tutorials</LinkWrapper></li>
                     <li><LinkWrapper to="/terms">Terms & Conditions</LinkWrapper></li>
                     <li><LinkWrapper to="/privacy">Privacy Policy</LinkWrapper></li>
                     <li><LinkWrapper to="/support">Support</LinkWrapper></li>
                     <li><LinkWrapper to="/about">About</LinkWrapper></li>
                 </ul>
-
-
-            <ul class="sidenav" id="mobile-demo">
-            <li><LinkWrapper to="/home">List of Tutorials</LinkWrapper></li>
-            <li><LinkWrapper to="/terms">Terms & Conditions</LinkWrapper></li>
-            <li><LinkWrapper to="/privacy">Privacy Policy</LinkWrapper></li>
-            <li><LinkWrapper to="/support">Support</LinkWrapper></li>
-            <li><LinkWrapper to="/about">About</LinkWrapper></li>
-        </ul>
-
             </div>
         </nav>
-
-
-
-
-
     );
 }
 
